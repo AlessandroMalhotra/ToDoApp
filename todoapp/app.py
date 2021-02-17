24101cc0ea722dba3070ae0d2b21670e6a69aa73
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-02-17 16:17:46 +0000

    Updates added delete and update functonality

--- todoapp/app.py
+++ todoapp/app.py
@@ -15,14 +15,23 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-  
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    child = db.relationship('Todo', backref='list', lazy=True)
 
+
+  
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
     error = False
@@ -43,9 +52,35 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todo/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/todo/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+
 @app.route('/')
 def index():
-   return render_template('index.html', data=Todo.query.all())
+   return render_template('index.html', todo=Todo.query.order_by('id').all())
 
 
 if __name__ == '__main__':
