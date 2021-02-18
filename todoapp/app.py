207dcae84915dcede7e90cc6ee36621c1cde2805
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-02-18 11:49:03 +0000

    Added route handler for seperate lists

--- todoapp/app.py
+++ todoapp/app.py
@@ -24,6 +24,7 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+
 class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
@@ -31,7 +32,6 @@ class TodoList(db.Model):
     child = db.relationship('Todo', backref='list', lazy=True)
 
 
-  
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
     error = False
@@ -78,10 +78,14 @@ def delete_todo(todo_id):
     return jsonify({'success': True})
 
 
+@app.route('/lists/<list_id>/')
+def get_list_todos(list_id):
+   return render_template('index.html', todo=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+
 @app.route('/')
 def index():
-   return render_template('index.html', todo=Todo.query.order_by('id').all())
-
+    return redirect(url_for('get_list_todos', list_id=1))
 
 if __name__ == '__main__':
     app.run()
